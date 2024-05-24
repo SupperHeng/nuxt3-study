1bdbe8a9ce9bc0256762c9c1752ebd25dfbdd746
symxll <symxll@126.com>
2024-05-24 15:57:23 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: 'less' // 配置为less
+          })
+        ],
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            'primary-color': '#ea6f5a'
+          },
+          javascriptEnabled: true,
+        }
+      }
+    },
+    ssr: {
+      noExternal: ['ant-design-vue']
+    }
+  }
 })
