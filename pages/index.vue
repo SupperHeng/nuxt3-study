3e46ab0fe3f682541c7a2f8c6c89a8a71fee5992
symxll <symxll@126.com>
2024-05-29 03:43:19 +0800

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -7,25 +7,34 @@
   <br />
   <a-button type="primary" @click="toAbout">jump to about</a-button>
   <br />
-  <NuxtLink :to="{path: '/about',query: { userInfo: JSON.stringify(userInfo) } }">Jump to about with parameters</NuxtLink>
-  
+  <NuxtLink
+    :to="{ path: '/about', query: { userInfo: JSON.stringify(userInfo) } }"
+    >Jump to about with parameters</NuxtLink
+  >
+  <br />
+  <!-- <h1>{{ $myPlugin("World") }}</h1> -->
+  <a-button type="primary" @click="info">Display normal message</a-button>
 </template>
 
-<script setup lang='ts'>
-const router = useRouter()
+<script setup lang="ts">
+const { $message } = useNuxtApp();
+
+const info = () => {
+  $message.info("This is a normal message");
+};
+
+const router = useRouter();
 const userInfo = ref({
   id: 1,
-  username: 'xiaoheng'
-})
-
+  username: "xiaoheng",
+});
 
 const toAbout = () => {
   router.push({
-    path: '/about',
-    query: { userInfo: JSON.stringify(userInfo.value) }
-  })
-}
-
+    path: "/about",
+    query: { userInfo: JSON.stringify(userInfo.value) },
+  });
+};
 </script>
 
 <style></style>
