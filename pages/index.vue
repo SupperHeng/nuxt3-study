d1627d7f36a85de2c8bd9be3a0219c34c98902d4
symxll <symxll@126.com>
2024-05-29 16:01:54 +0800

    useState and useCookie
    This class learned about useState and useCookie

--- pages/index.vue
+++ pages/index.vue
@@ -12,15 +12,41 @@
     >Jump to about with parameters</NuxtLink
   >
   <br />
+
+  <!-- Pinia -->
   <!-- <h1>{{ $myPlugin("World") }}</h1> -->
   <a-button type="primary" @click="info">Display normal message</a-button>
   <h1>counter: {{ myStore.counter }}</h1>
   <h1>counter: {{ myStore.doubleCounter }}</h1>
   <a-button @click="myStore.add()">increment</a-button>
+
+  <!-- useState -->
+  <h2>counter:{{ counter }}</h2>
+  <a-button @click="add()">add</a-button>
 </template>
 
 <script setup lang="ts">
+// useCookie
+const userInfoCookie = useCookie('userInfo', {maxAge: 60})
+userInfoCookie.value = {
+  uid: 333,
+  username: 'xiaoheng'
+}
 
+const userinfo = useCookie('userInfo')
+console.log("userInfo",userinfo.value);
+
+
+// useState
+// const counter =  useState('counter', () => 1)
+// counter.value = 7
+const counter = useCounter();
+counter.value = 17;
+const add = () => {
+  counter.value++
+}
+
+// Pinia
 // 引入myStore
 import { useMyStore } from "~/store/myStore";
 const myStore = useMyStore();
